ba4f2a647c6e96e0d6a4e447cbacf705e113c8c7
Somesh Teja Yerramsetty <someshteja.yerramsetty@gmail.com>
2024-12-27 12:48:16 +0100

    Leetcode Reverse Integer

--- leetcode_7.cpp
+++ leetcode_7.cpp
@@ -1,22 +1,22 @@
 #include<iostream>
 #include<math.h>
 using namespace std;
+int main() {
 
-//Decimal to Binary
-int main(){
-    int n;
-    cin>>n;
-    int rem=0;
-    int ans=0;
-    int place=1;
-    while(n!=0){
-
-        rem=n%2;
-        ans=rem * place+ans;
-      
-        n=n/2;
-        place*=10;
-   
-    }
-    cout<<ans<< " This is the binary "<<endl;
-} 
+        int n;
+        cin>>n;
+        int digit=0;
+        int ans=0;
+        while(n!=0){
+            digit=n%10;
+            if (ans >= -214748364 && ans <= 214748364) {
+             ans = ans * 10 + digit;
+            }
+            else {
+               ans = 0;
+               break;
+            }
+            n=n/10;
+        }
+        cout<< ans<<" is the Reversed Integer";
+    };
